f0fff2791b82b6a9c2f29ae4b897f7da3bd9cecd
dmrat <dmratc@dmratcliffe.com>
2018-04-17 12:25:52 -0500

    Initial commit

--- Wox.Plugin.Torrent/Main.cs
+++ Wox.Plugin.Torrent/Main.cs
@@ -25,15 +25,21 @@ namespace Wox.Plugin.PirateBay
                 {
                     try
                     {
-                        int num1 = 0;
-                        string pgsrc = webClient.DownloadString(APISEARCH + query.Search);
-                        pgsrc = pgsrc.Replace("\n", "").Replace("\r", "").Replace("\t", "");
-                        MatchCollection results = Regex.Matches(pgsrc, "<tr.*?title=\"Details for (.*?)\".*?<a href=\"magnet:\\?(.*?)\".*?, Size (.*?),.*?<td align=\"right\">(.*?)<\\/td><td align=\"right\">(.*?)<\\/td>.*?<\\/tr>");
+                        int numTorrents = 0;
+                        //Get the HTML of pirate bay... we're going to parse it with regex...
+                        string pbHTML = webClient.DownloadString(APISEARCH + query.Search);
+                        //Fix some formatting
+                        pbHTML = pbHTML.Replace("\n", "").Replace("\r", "").Replace("\t", "");
+                        //Collection of results, it takes the title, magnet, and details about activity.
+                        MatchCollection results = Regex.Matches(pbHTML, 
+                            "<tr.*?title=\"Details for (.*?)\".*?<a href=\"magnet:\\?(.*?)\".*?, Size (.*?),.*?<td align=\"right\">(.*?)<\\/td><td align=\"right\">(.*?)<\\/td>.*?<\\/tr>");
                         foreach (Match m in results)
                         {
-                            if (num1 < MAXRESULTS)
+                            //only show MAXRESULTS number of torrents. TODO: Make this a Wox based setting
+                            if (numTorrents < MAXRESULTS)
                             {
                                 string itemName = m.Groups[1].ToString();
+                                //We ruined the formating of the magnet, to get our torrent client to open we need to reformat it.
                                 string itemMagnet = "magnet:?" + m.Groups[2].ToString();
 
                                 string itemSize = m.Groups[3].ToString().Replace("&nbsp;","");
@@ -41,8 +47,10 @@ namespace Wox.Plugin.PirateBay
                                 string itemSeeds = m.Groups[4].ToString();
                                 string itemLeech = m.Groups[5].ToString();
 
+                                //the final information
                                 string subString = String.Format("Seeders: {0} Leechers: {1} Size: {2}", itemSeeds, itemLeech, itemSize);
 
+                                //not sure why this is so complicated
                                 List<Result> tempList = mainList;
                                 Result torrent = new Result();
                                 torrent.Title = itemName;
@@ -54,13 +62,15 @@ namespace Wox.Plugin.PirateBay
                                     return true;
                                 }));
 
+                                //why do I make a new result? take this out and double check it works....
                                 Result result2 = torrent;
                                 tempList.Add(result2);
-                                ++num1;
+                                ++numTorrents;
                             }
                         }
 
-                        if (num1 == 0)
+                        //There wasn't anything to be found (This isn't an error! It should not be treated as such)
+                        if (numTorrents == 0)
                         {
                             List<Result> list2 = mainList;
                             Result result1 = new Result();
@@ -72,6 +82,7 @@ namespace Wox.Plugin.PirateBay
                             list2.Add(result2);
                         }
                     }
+                    //Display the error if we got one (Usually this means you had trouble with connecting to TPB)
                     catch (Exception ex)
                     {
                         List<Result> list2 = mainList;
